35c22d036974eccced92c26d69a40f8ae2e07b29
Julien Finet <julien.finet@kitware.com>
2014-05-07 16:59:47 -0400

    Change GetInputConnection error message into a debug warning message
    Change-Id: Ide8b69796a73b910d877a9c716de9e93f63fa29b

--- Common/ExecutionModel/vtkAlgorithm.cxx
+++ Common/ExecutionModel/vtkAlgorithm.cxx
@@ -1419,10 +1419,12 @@ vtkAlgorithmOutput* vtkAlgorithm::GetInputConnection(int port, int index)
 {
   if(index < 0 || index >= this->GetNumberOfInputConnections(port))
     {
-    vtkErrorMacro("Attempt to get connection index " << index
-                  << " for input port " << port << ", which has "
-                  << this->GetNumberOfInputConnections(port)
-                  << " connections.");
+#if !defined NDEBUG
+    vtkWarningMacro("Attempt to get connection index " << index
+                    << " for input port " << port << ", which has "
+                    << this->GetNumberOfInputConnections(port)
+                    << " connections.");
+#endif
     return 0;
     }
   if(vtkInformation* info =
