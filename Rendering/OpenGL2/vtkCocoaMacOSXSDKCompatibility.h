f1a8aec13dbac87f016872da37d96529eec088d6
Max Smolens <max.smolens@kitware.com>
2016-07-06 10:57:00 -0400

    Revert "Increase VTK OS X minimum check from 10.6.0 to 10.7.0"
    This reverts commits 9e612e6ddc4ad926dfed4140eebe37be4a6ce748 and
    f2e249c0a6a93f12808822cc5e5dddda35a09a35
    
    This fixes the following error when CMAKE_OSX_DEPLOYMENT_TARGET is set to 10.6:
    
        In file included from /path/to/Slicer-build/VTKv7/Rendering/OpenGL/vtkCocoaRenderWindowInteractor.mm:17:
        /path/to/Slicer-build/VTKv7/Rendering/OpenGL/vtkCocoaMacOSXSDKCompatibility.h:29:4: error: VTK requires a deployment target of Mac OS X 10.6.8 or later
          #error VTK requires a deployment target of Mac OS X 10.6.8 or later
    
    A future solution could be to modify CMake to allow specifying a
    CMAKE_OSX_DEPLOYMENT_TARGET with a revision, such as 10.6.8. As of CMake 3.5.2,
    that fails with the following error at configuration time:
    
        CMake Error at /path/to/CMake.app/Contents/share/cmake-3.5/Modules/Platform/Darwin.cmake:84 (message):
          CMAKE_OSX_DEPLOYMENT_TARGET (10.6.8) is greater than CMAKE_OSX_SYSROOT SDK:
    
           /Developer/SDKs/MacOSX10.6.sdk
    
          Please set CMAKE_OSX_DEPLOYMENT_TARGET to 10.6 or lower.
    
    See more information in the merge request containing the original commit:
    https://gitlab.kitware.com/vtk/vtk/merge_requests/1405.
    
    Co-authored-by: Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>

--- Rendering/OpenGL2/vtkCocoaMacOSXSDKCompatibility.h
+++ Rendering/OpenGL2/vtkCocoaMacOSXSDKCompatibility.h
@@ -24,12 +24,17 @@ PURPOSE.  See the above copyright notice for more information.
 
 #include <AvailabilityMacros.h>
 
-#if MAC_OS_X_VERSION_MAX_ALLOWED < 1070
-  #error VTK requires the Mac OS X 10.7 SDK or later
+#if MAC_OS_X_VERSION_MAX_ALLOWED < 1060
+  #error VTK requires the Mac OS X 10.6 SDK or later
 #endif
 
-#if MAC_OS_X_VERSION_MIN_REQUIRED < 1070
-  #error VTK requires a deployment target of Mac OS X 10.7 or later
+#if MAC_OS_X_VERSION_MIN_REQUIRED < 1060
+  #error VTK requires a deployment target of Mac OS X 10.6 or later
+#endif
+
+// __has_feature is new in the 10.7 SDK, define it here if it's not yet defined.
+#ifndef __has_feature
+  #define __has_feature(x) 0
 #endif
 
 #if (MAC_OS_X_VERSION_MAX_ALLOWED < 101200) && !defined(VTK_DONT_MAP_10_12_ENUMS)
